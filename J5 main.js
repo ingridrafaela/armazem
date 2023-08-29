2925c3c0108eb45c4012d016139284086a95c007
ingridrafaela <105295591+ingridrafaela@users.noreply.github.com>
2023-08-29 09:00:05 -0300

    .

--- J5 main.js
+++ J5 main.js
@@ -10,3 +10,7 @@ function dizOi(nome) {
 }
 
 dizOi("Pedro") 
+function mudaCores(elementos){
+    var cores = elementos.getAttribute("[data-cor]");
+    var tipoElemento = elementos.getAttribute("[data-tipo]");
+  }
